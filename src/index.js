f954bc4c008e258e396f42816a672d6151451f47
coonghe03 <jdcoonghe03@gmail.com>
2026-04-26 09:07:33 +0530

    deni

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// src/index.js
+//src/index.js
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-// import notificationRoutes from "./routes/notificationRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import { swaggerUi, swaggerSpec } from "./swagger.js";
 
 dotenv.config();
 
@@ -11,8 +12,9 @@ const PORT = process.env.PORT || 3004;
 
 app.use(express.json());
 
-// // Routes
-// app.use("/api/notifications", notificationRoutes);
+// Routes
+app.use("/api/notifications", notificationRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Health check
 app.get("/health", (req, res) => {
---
+++ src/swagger.js
@@ -0,0 +1,38 @@
+// src/swagger.js
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Notification Service API",
+      version: "1.0.0",
+      description: "Smart Event Notification Service API documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3004",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"], // read route files
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export { swaggerUi, swaggerSpec };
